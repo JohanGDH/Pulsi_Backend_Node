a0fa1f58f820d5a07e6630be94a690e2a94931c7
JohanGDH <jgiandh46@gmail.com>
2024-08-12 22:09:37 -0500

    Iniciada migración a TypeScript

---
+++ API/controllers/pulsi_controller.js
@@ -0,0 +1,33 @@
+const pulsi_model = require("../models/data_model");
+
+
+const controller = {
+
+
+    getPulsiByID: (req, res) => {
+
+        const pulsi_ID = req.params.pulsi_ID;
+
+        if (pulsi_ID ? !pulsi_ID : !pulsi_ID.trim()) {
+            return res.status(400).send({ message: "El ID del pulsi es requerido" });
+        };  
+
+        pulsi_model.findOne({ pulsi_ID: pulsi_ID }, (err, pulsi) => {
+
+            if (err) {
+                return res.status(500).send({ message: "Error al buscar los datos" });
+            };
+
+            if (!pulsi) {
+                return res.status(404).send({ message: "No se han encontrado los datos" });
+            };
+
+            return res.status(200).send({ 
+                pulsi
+
+            });
+
+        });
+    }
+
+}
